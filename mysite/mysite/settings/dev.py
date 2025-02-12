f6b7c550c75dcae5b79a199bc777ad308a6f96b9
notwld <mwfarrukh@gmail.com>
2025-02-12 21:40:45 +0500

    changes made

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-2@hs6(ne$c89u@!^)mgsy272zg#k@gma4pv*=fjmi$7s_+(ley"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
